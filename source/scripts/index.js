dbe69805e94e076d591fe7a75468d5842443ed2f
KaterinaLarionova <lovadza@mail.ru>
2024-09-12 21:12:05 +0300

    добавление меню - гамбургер

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,14 @@
 /* в этот файл добавляет скрипты*/
+let navMain = document.querySelector('.site-list');
+let navToggle = document.querySelector('.main-header__toggle');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav__list--closed')) {
+    navMain.classList.remove('main-nav__list--closed');
+    navMain.classList.add('main-nav__list');
+  } else {
+    navMain.classList.add('main-nav__list--closed');
+    navMain.classList.remove('main-nav__list');
+  }
+});
+
