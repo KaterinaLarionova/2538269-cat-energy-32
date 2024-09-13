4b145b4ba0a4963d5296298b317b89a292a9f87e
KaterinaLarionova <lovadza@mail.ru>
2024-09-13 10:13:33 +0300

    исправление в js

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -4,12 +4,16 @@ let navMain = document.querySelector('.site-list');
 let navToggle = document.querySelector('.main-header__toggle');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav__list--closed')) {
-    navMain.classList.remove('main-nav__list--closed');
-    navMain.classList.add('main-nav__list');
-  } else {
-    navMain.classList.add('main-nav__list--closed');
+  if (navToggle.classList.contains('main-header__toggle')) {
     navMain.classList.remove('main-nav__list');
+    navMain.classList.add('main-nav__list--closed');
+    navToggle.classList.remove('main-header__toggle');
+    navToggle.classList.add('main-header__toggle--closed');
+  } else {
+    navMain.classList.add('main-nav__list');
+    navMain.classList.remove('main-nav__list--closed');
+    navToggle.classList.add('main-header__toggle');
+    navToggle.classList.remove('main-header__toggle--closed');
   }
 });
 
